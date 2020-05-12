0ff2be24544c35721bf1eccda4999b00d40f9659
AaravRajput <aarav.rajput2003@gmail.com>
2020-05-12 16:22:29 -0700

    Work Started on main app

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
+def readTable():
+    rec=[]
+    with open('PRISONDATA.csv') as pD:
+        reco=csv.reader(pD)
+        for i in reco:
+            rec.append(i)
+        rec.pop(0)
+        rec.pop(-1)
+    return rec
 
 @app.route('/')
 def home_page():
@@ -13,7 +23,7 @@ def contact_page():
 
 @app.route('/app')
 def app_page():
-    return render_template('app.html')
+    return render_template('app.html',value=readTable())
 
 @app.route('/about')
 def about_page():
